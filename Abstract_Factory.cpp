7f57dee7830253623095b75bf371d53e862d57b0
Tony <tonyboboc6@gmail.com>
2025-07-08 14:19:26 +0300

    finished Abstract Factory

--- Abstract_Factory.cpp
+++ Abstract_Factory.cpp
@@ -23,7 +23,7 @@ class NintendoController :public BaseController{
     public:
     ~NintendoController()=default;
     std::string getbrand()const override{
-        return "nintendo";
+        return "Nintendo";
     }
 };
 class Cd{
@@ -49,7 +49,7 @@ class NintendoCd:public Cd{
     public:
     ~NintendoCd()=default;
     std::string getbrand()const override{
-        return "nintendo";
+        return "Nintendo";
     }
 };
 class BaseConsole {
@@ -59,8 +59,8 @@ public:
         return"playing on base console";
     };
     virtual std::string getbrand()const =0;
-    virtual std::string plugcontroller(BaseController& con)const=0;
-    virtual std::string putgame(Cd& cd)const=0;
+    virtual std::string plugcontroller(const BaseController& con)const=0;
+    virtual std::string putgame(const Cd& cd)const=0;
 };
 class SonyConsole:public BaseConsole{
   public :
@@ -71,13 +71,13 @@ class SonyConsole:public BaseConsole{
   std::string getbrand()const override{
     return "sony";
   } 
-  std::string plugcontroller(BaseController& con)const override{
+  std::string plugcontroller(const BaseController& con)const override{
     if(!(this->getbrand()==con.getbrand())){
         return "doesn't work , not same brand";
     }
     return "playing on Sony Console with Sony Controller";
   }
-  std::string putgame(Cd& cd)const override{
+  std::string putgame(const Cd& cd)const override{
     if(!(this->getbrand()==cd.getbrand())){
         return "doesn't work , not same brand";
     }
@@ -93,13 +93,13 @@ return "playing on Xbox Console";
  }std::string getbrand()const override{
     return "xbox";
   } 
-  std::string plugcontroller(BaseController& con)const override{
+  std::string plugcontroller(const BaseController& con)const override{
     if(!(this->getbrand()==con.getbrand())){
         return "doesn't work , not same brand";
     }
     return "playing on Xbox Console with Xbox Controller";
   }
-  std::string putgame(Cd& cd)const override{
+  std::string putgame(const Cd& cd)const override{
     if(!(this->getbrand()==cd.getbrand())){
         return "doesn't work , not same brand";
     }
@@ -113,13 +113,13 @@ return "playing on Nintendo Console";
  }std::string getbrand()const override{
     return "Nintendo";
   } 
-  std::string plugcontroller(BaseController& con)const override{
+  std::string plugcontroller(const BaseController& con)const override{
     if(!(this->getbrand()==con.getbrand())){
         return "doesn't work , not same brand";
     }
     return "playing on Nintendo Console with Nintendo Controller";
   }
-  std::string putgame(Cd& cd)const override{
+  std::string putgame(const Cd& cd)const override{
     if(!(this->getbrand()==cd.getbrand())){
         return "doesn't work , not same brand";
     }
@@ -127,5 +127,69 @@ return "playing on Nintendo Console";
   }
 };
 class AbstractFactory {
-
+    public:
+virtual ~AbstractFactory()=default;
+virtual BaseConsole* createConsole()const=0;
+virtual BaseController* createController()const=0;
+virtual Cd* createGame()const=0;
 };
+class SonyFactory :public AbstractFactory{
+    public:
+~SonyFactory()=default;
+BaseConsole* createConsole()const override {
+    return new SonyConsole();
+}
+BaseController* createController()const override{
+    return new SonyController();
+}
+Cd* createGame()const override{
+    return new SonyCd();
+}
+};
+class XboxFactory :public AbstractFactory{
+    public:
+~XboxFactory()=default;
+BaseConsole* createConsole()const override {
+    return new XboxConsole();
+}
+BaseController* createController()const override{
+    return new XboxController();
+}
+Cd* createGame()const override{
+    return new XboxCd();
+}
+};
+class NintendoFactory :public AbstractFactory{
+    public:
+~NintendoFactory()=default;
+BaseConsole* createConsole()const override {
+    return new NintendoConsole();
+}
+BaseController* createController()const override{
+    return new NintendoController();
+}
+Cd* createGame()const override{
+    return new NintendoCd();
+}
+};
+void Client(const AbstractFactory & x){
+    BaseConsole* cons=x.createConsole();
+    BaseController* cont=x.createController();
+    Cd* cd=x.createGame();
+    std::cout<<cons->plugcontroller(*cont)<<std::endl;
+    std::cout<<cons->putgame(*cd)<<std::endl;
+    std::cout<<cons->play()<<std::endl;
+    delete cons;delete cont;delete cd;
+}
+int main(){
+AbstractFactory* a=new SonyFactory();
+Client(*a);
+delete a;
+a=new NintendoFactory();
+Client(*a);
+delete a;
+a=new XboxFactory();
+Client(*a);
+delete a;
+return 0;
+}
