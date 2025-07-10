693f59ede3a134f8a5bdb9496ae49e08f11e43c6
Tony <tonyboboc6@gmail.com>
2025-07-10 15:52:38 +0300

    added a second builder named manual

--- Builder.cpp
+++ Builder.cpp
@@ -23,11 +23,59 @@ class Console {
     }
     ~Console()=default;
 };
+class Manual{
+    private:
+    std::string publisher;
+    std::string storage;
+    public:
+    Manual()=default;
+    void setstorage(const std::string & c){
+        storage=c;
+    }
+    void setpublisher(const std::string &c){
+        publisher=c;
+    }
+    std::string browsing (){
+        return "if you want to browse the games, go to the game library in the console menu";
+    }
+    std::string play(){
+        return "if you wanna play games, put the game discs into the console";
+    }
+    std::string info(){
+        return "this is a console made by "+publisher+" and it has a storage of "+storage+"\n";
+    }
+    ~Manual()=default;
+};
 class Builder{
     public:
     virtual ~Builder()=default;
     virtual void producepublisher(const std::string&c)=0;
     virtual void producestorage(const std::string&c)=0;
+};
+class ManualBuilder:public Builder{
+    private:
+    Manual* manual;
+    public:
+    ~ManualBuilder(){
+        delete manual;
+    }
+    ManualBuilder(){
+        this->reset();
+    }
+    void reset(){
+        this->manual=new Manual;
+    }
+    Manual * getprod(){
+        Manual * m=manual;
+        this->reset();
+        return m;
+    }
+    void producepublisher(const std::string &c) override{
+        manual->setpublisher(c);
+    }
+    void producestorage(const std::string &c)override{
+        manual->setstorage(c);
+    }
 };
 class ConsoleBuilder:public Builder{
     private:
@@ -64,7 +112,7 @@ class Director{
     void setbuilder (Builder * builder){
         this->builder=builder;
     }
-    void buildconsole(const std::string &c,const std::string& s){
+    void build(const std::string &c,const std::string& s){
         builder->producepublisher(c);
         builder->producestorage(s);
     }
@@ -79,18 +127,33 @@ void clientcode(Director & director){
     std::cin>>s1;
     std::cout<<"how much space in it : ";
     std::cin>>s2;
-    director.buildconsole(s1,s2);
+    director.build(s1,s2);
     Console* c=conbuilder->getprod();
     std::cout<<c->info();
     delete c;
-//Created using only builder (it is easier to use director if you have another builder class (ex another for a console manual))
+//Created using only builder 
     conbuilder->producepublisher(s1);
     conbuilder->producestorage(s2);
     c=conbuilder->getprod();
     std::cout<<c->info();
     delete c;
     delete conbuilder;
+//Creating Manual using director
+    ManualBuilder *  manbuilder=new ManualBuilder(); 
+    director.setbuilder(manbuilder);
+    director.build(s1,s2);
+    Manual * man=manbuilder->getprod();
+    std::cout<<man->info();
+//Creating Manual using Builder
+    delete man;
+    manbuilder->producepublisher(s1);   
+    manbuilder->producestorage(s2);
+    man=manbuilder->getprod();
+    std::cout<<man->info();
+    delete man ;
+    delete manbuilder;
 }
+//It is easier using a director since we don't need to tell the builder what to build each and every time
 int main(){
 
     Director d;
