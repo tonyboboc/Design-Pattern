8eed26773547047aedb2d394ebdb08efe93602b1
Tony <tonyboboc6@gmail.com>
2025-07-11 17:06:42 +0300

    finished prototype

--- Prototype.cpp
+++ Prototype.cpp
@@ -7,7 +7,7 @@ enum class type{
     Mage
 };
 class Character{
-    private :
+    protected :
     int health;
     std::string name;
     int stamina;
@@ -30,6 +30,7 @@ class Character{
     virtual std::string return_name()const {
         return this->name;
     }
+    virtual std::string info()const=0;
 };
 class Warrior:public Character{
     private:
@@ -44,6 +45,9 @@ class Warrior:public Character{
     } 
     std::unique_ptr<Character> Clone()const override{
         return std::make_unique<Warrior>(*this);
+    }
+    std::string info()const override{
+        return "Warrior with stats : "+std::to_string(health)+" "+name+" "+std::to_string(stamina)+" "+std::to_string(walkspeed)+" "+weapon;
     }
 };
 class Mage :public Character{
@@ -60,4 +64,40 @@ class Mage :public Character{
     std::unique_ptr<Character> Clone()const override{
         return std::make_unique<Mage>(*this);
     }
+    std::string info()const override{
+        return "Warrior with stats : "+std::to_string(health)+" "+name+" "+std::to_string(stamina)+" "+std::to_string(walkspeed)+" "+spell;
+    }
 };
+class CharacterFactory{
+    std::unordered_map<type,std::unique_ptr<Character>> characters;
+    public:
+        CharacterFactory(){
+            characters[type::Warrior]= std::make_unique<Warrior>("basic weapon","Warrior",100,120,50);
+            characters[type::Mage]=std::make_unique<Mage>("basic spell","Mage",85,100,75);
+        }
+        ~CharacterFactory()=default;//Because we used smart pointers
+        std::unique_ptr<Character> createcharacter(type Type){
+            return (characters[Type]->Clone());
+        }
+};
+void Client (CharacterFactory & factory){
+    std::cout<<"what character type do you want ? mage(0) or warrior(1) ?";
+    int x;
+    std::cin>>x;
+    std::unique_ptr<Character> character;
+    if(x==0){
+        character=factory.createcharacter(type::Mage);
+        std::cout<<character->info();
+    }
+    else{
+        character=factory.createcharacter(type::Warrior);
+        std::cout<<character->info();
+    }
+    //no need for deletion since we use smart pointers
+
+}
+int main(){
+    CharacterFactory factory;
+    Client(factory);
+    return 0;
+}
