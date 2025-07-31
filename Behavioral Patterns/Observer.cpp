c8cbd1c5692a3fccc6b2f3278c800ececeb27870
Boboc George <tonyboboc6@gmail.com>
2025-07-31 19:11:39 +0300

    made Subject

--- Behavioral Patterns/Observer.cpp
+++ Behavioral Patterns/Observer.cpp
@@ -13,3 +13,30 @@ public:
     virtual void remove(BaseObserver* obs)=0;
     virtual void Notify()=0;
 };
+class Subject:public BaseSubject{
+    private:
+    std::list<BaseObserver*> observers;
+    std::string message;
+    public:
+    ~Subject(){
+        std::cout<<"Deletion of subject\n";
+    }
+    void attach(BaseObserver* obs)override{
+        observers.push_back(obs);
+    }
+    void remove(BaseObserver* obs)override{
+        observers.remove(obs);
+    }
+    void Notify()override{
+        std::list<BaseObserver*>::iterator it=observers.begin();
+        while(it!=observers.end()){
+            (*it)->Update(message);
+            ++it;
+        }
+
+    }
+    void CreateMessage(const std::string &msg){
+        message=msg;
+        Notify();
+    }
+};
