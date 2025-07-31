860365552e98552d3c2fb48fd0e674b89ac6b821
Boboc George <tonyboboc6@gmail.com>
2025-07-31 18:39:13 +0300

    made Abstract classes

--- Behavioral Patterns/Observer.cpp
+++ Behavioral Patterns/Observer.cpp
@@ -1,3 +1,15 @@
 #include <list>
 #include <string>
 #include <iostream>
+class BaseObserver{
+    public:
+virtual  ~BaseObserver()=default;
+virtual void Update(const std::string & message)=0;
+};
+class BaseSubject{
+public:
+    virtual ~BaseSubject()=default;
+    virtual void attach(BaseObserver* obs)=0;
+    virtual void remove(BaseObserver* obs)=0;
+    virtual void Notify()=0;
+};
