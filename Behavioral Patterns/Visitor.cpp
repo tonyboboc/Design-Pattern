7d9da075f49eb85d9011cbacfafc7d15ba3b60df
Boboc George <tonyboboc6@gmail.com>
2025-08-04 22:40:03 +0300

    made Console and Pc

--- Behavioral Patterns/Visitor.cpp
+++ Behavioral Patterns/Visitor.cpp
@@ -3,6 +3,7 @@
 class Console;
 class Pc;
 class BaseVisitor{
+    public:
     void VisitConsole(const Console* console);
     void VisitPC(const Pc* pc);
 };
@@ -10,4 +11,22 @@ class Component{
     public:
     virtual ~Component()=default;
     virtual void Accept(BaseVisitor *visitor) const = 0;
+};
+class Console:public Component{
+    public:
+    void Accept(BaseVisitor * visitor)const{
+        visitor->VisitConsole(this);
+    }
+    void ConsoleStuff(){
+        std::cout<<"Doing stuff you can only do on console (play games from  ps+)";
+    }
+};
+class Pc:public Component{
+public:
+    void Accept(BaseVisitor * visitor)const{
+        visitor->VisitPC(this);
+    }
+    void PcStuff(){
+        std::cout<<"Doing stuff you can only do on pc (pay taxes)";
+    }
 };
