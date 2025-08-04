68900fb1542b76e43fa14db4eac604bff0c97355
Boboc George <tonyboboc6@gmail.com>
2025-08-04 22:21:31 +0300

    made Base Component

--- Behavioral Patterns/Visitor.cpp
+++ Behavioral Patterns/Visitor.cpp
@@ -6,3 +6,8 @@ class BaseVisitor{
     void VisitConsole(const Console* console);
     void VisitPC(const Pc* pc);
 };
+class Component{
+    public:
+    virtual ~Component()=default;
+    virtual void Accept(BaseVisitor *visitor) const = 0;
+};
