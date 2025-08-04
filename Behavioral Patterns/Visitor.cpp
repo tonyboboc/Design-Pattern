ac725860152bdd9ec4886fcdb872d756ac16f3c3
Boboc George <tonyboboc6@gmail.com>
2025-08-04 23:01:30 +0300

    finished Visitor.cpp and all the dessign patterns from the book I am currently reading

--- Behavioral Patterns/Visitor.cpp
+++ Behavioral Patterns/Visitor.cpp
@@ -4,8 +4,8 @@ class Console;
 class Pc;
 class BaseVisitor{
     public:
-    void VisitConsole(const Console* console);
-    void VisitPC(const Pc* pc);
+    virtual void VisitConsole(const Console* console)const=0;
+    virtual void VisitPc(const Pc* pc)const=0;
 };
 class Component{
     public:
@@ -17,16 +17,36 @@ class Console:public Component{
     void Accept(BaseVisitor * visitor)const{
         visitor->VisitConsole(this);
     }
-    void ConsoleStuff(){
-        std::cout<<"Doing stuff you can only do on console (play games from  ps+)";
+    std::string ConsoleStuff()const {
+        return"Doing stuff you can only do on console (play games from  ps+)";
     }
 };
 class Pc:public Component{
 public:
     void Accept(BaseVisitor * visitor)const{
-        visitor->VisitPC(this);
+        visitor->VisitPc(this);
     }
-    void PcStuff(){
-        std::cout<<"Doing stuff you can only do on pc (pay taxes)";
+    std::string PcStuff()const {
+        return"Doing stuff you can only do on pc (pay taxes)";
     }
 };
+class ConcreteVisitor:public BaseVisitor{
+    public:
+    void VisitConsole(const Console* console)const override{
+        std::cout<<console->ConsoleStuff()<<std::endl;
+    }
+    void VisitPc(const Pc* pc )const override{
+        std::cout<<pc->PcStuff()<<std::endl;
+    }
+};
+void ClientCode(){
+    ConcreteVisitor visitor;
+    Component * pc=new Pc();
+    Component* console=new Console();
+    pc->Accept(&visitor);
+    console->Accept(&visitor);
+ }
+ int main(){
+    ClientCode();
+    return 0;
+ }
